f5e3e2cbc30577f54df0deadf2543133130618b7
Rafal <rpulawski84@gmail.com>
2019-02-04 00:50:20 +0100

    zrobiony Palindrom

--- src/main/java/Palindrom.java
+++ src/main/java/Palindrom.java
@@ -5,13 +5,31 @@ public class Palindrom {
         checkIfTextIsPalindrom("Kajak");
         checkIfTextIsPalindrom("kjak");
         checkIfTextIsPalindrom("A to kanapa pana Kota");
+        checkWithoutApache("Kajak");
+        checkWithoutApache("Kjak");
+        checkWithoutApache("A to kanapa pana Kota");
     }
 
     private static void checkIfTextIsPalindrom(String text) {
-        String textnew = text.toLowerCase().replaceAll(" ","");
+        String textnew = text.toLowerCase().replaceAll(" ", "");
         String reverse = StringUtils.reverse(textnew);
-        if (reverse.equals(textnew)){
+        if (reverse.equals(textnew)) {
             System.out.println("ok");
+        } else {
+            System.out.println("nie jest");
+        }
+    }
+
+    private static void checkWithoutApache(String text) {
+        String preparedText = text.toLowerCase().replaceAll(" ","");
+        int counter = 0;
+        for (int i = 0; i < preparedText.length(); i++) {
+            if ( preparedText.charAt(i)== preparedText.charAt(preparedText.length()-i -1)) {
+                counter++;
+            }
+        }
+        if(counter == preparedText.length()){
+            System.out.println("jest");
         }else{
             System.out.println("nie jest");
         }
